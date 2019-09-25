4a62650c7a3556552ef92cf31450f848d83180c6
Moyan <ice_moyan@163.com>
2019-09-25 20:16:34 +0800

    add code

--- my_config/skudet/skudet_cascade_rcnn_r50_fpn_multiscale_finetuningsku110k_2x_190925.py
+++ my_config/skudet/skudet_cascade_rcnn_r50_fpn_multiscale_finetuningsku110k_2x_190925.py
@@ -228,7 +228,7 @@ log_config = dict(
 total_epochs = 21
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/skudet_cascade_rcnn_r50_fpn_multiscale_2x_190925'
+work_dir = './work_dirs/skudet_cascade_rcnn_r50_fpn_multiscale_finetuningsku110k_2x_190925'
 load_from = './work_dirs/sku110k/sku110k_cascade_rcnn_r50_fpn_multiscale_2x_190925/epoch_21.pth'
 resume_from = None
 workflow = [('train', 1)]
