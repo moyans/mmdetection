4bd6c0f5164ee05646e6e76468ff89799fe8f7ef
Moyan <ice_moyan@163.com>
2019-11-20 14:16:19 +0800

    fix the out of memory erro when gtbbox is too much

--- mmdet/core/bbox/assigners/max_iou_assigner.py
+++ mmdet/core/bbox/assigners/max_iou_assigner.py
@@ -72,6 +72,17 @@ class MaxIoUAssigner(BaseAssigner):
         """
         if bboxes.shape[0] == 0 or gt_bboxes.shape[0] == 0:
             raise ValueError('No gt or bboxes')
+        
+        # https://github.com/open-mmlab/mmdetection/pull/583
+        # move the assign operation to cpu
+        if gt_bboxes.shape[0] > 0:
+            bboxes = bboxes.cpu()
+            gt_bboxes = gt_bboxes.cpu()
+            if gt_bboxes_ignore is not None:
+                gt_bboxes_ignore = gt_bboxes_ignore.cpu()
+            if gt_labels is not None:
+                gt_labels = gt_labels.cpu()
+
         bboxes = bboxes[:, :4]
         overlaps = bbox_overlaps(gt_bboxes, bboxes)
 
@@ -88,6 +99,14 @@ class MaxIoUAssigner(BaseAssigner):
             overlaps[:, ignore_max_overlaps > self.ignore_iof_thr] = -1
 
         assign_result = self.assign_wrt_overlaps(overlaps, gt_labels)
+
+        # https://github.com/open-mmlab/mmdetection/pull/583
+	    # move the assign results back to cuda		
+        assign_result.gt_inds = assign_result.gt_inds.cuda()		
+        assign_result.max_overlaps = assign_result.max_overlaps.cuda()		
+        if assign_result.labels is not None:		
+            assign_result.labels = assign_result.labels.cuda()
+
         return assign_result
 
     def assign_wrt_overlaps(self, overlaps, gt_labels=None):
