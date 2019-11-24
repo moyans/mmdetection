bcb3f5efc45886159c5a20d83644b166f9a5c1c2
Moyan <ice_moyan@163.com>
2019-11-24 22:25:17 +0800

    update senet backbone

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -3,9 +3,10 @@ from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .efficientnet import EfficientNet
-from .simple_convnet import ConvnetLprVehicle, 
+from .simple_convnet import ConvnetLprVehicle
+from .senet import SENet
 
 # __all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet']
 
 __all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet',
-            'EfficientNet', 'ConvnetLprVehicle', 'ConvnetLprPlate']
+            'EfficientNet', 'ConvnetLprVehicle', 'ConvnetLprPlate', 'SENet']
