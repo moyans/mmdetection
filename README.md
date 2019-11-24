e6aaf2106f25caf808e60f195e085864dc0b22a7
Moyan <ice_moyan@163.com>
2019-11-24 18:39:43 +0800

    update res18 skudet config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+### test e2e
 Test Faster R-CNN and show the results.
 python tools/test.py configs/faster_rcnn_r50_fpn_1x.py \
     checkpoints/faster_rcnn_r50_fpn_1x_20181010-3d1b3351.pth \
@@ -12,8 +12,10 @@ Test Mask R-CNN with 8 GPUs, and evaluate the bbox and mask AP.
     checkpoints/mask_rcnn_r50_fpn_1x_20181010-069fa190.pth \
     8 --out results.pkl --eval bbox segm
 
+### test RPN
+ ./tools/dist_test.sh ${RPN_CONFIG_FILE} ${CHECKPOINT_FILE} ${GPU_NUM} [--out ${RESULT_FILE}] --eval proposal_fast
 
-# moyan update 
+### moyan update 
 fix the out of memory erro when gtbbox is too much @ https://github.com/open-mmlab/mmdetection/issues/188
 修改了/mmdet/core/bbox/geometry.py文件，将IoU的计算搬到了cpu上，能大幅度减少训练显存，但会使得训练速度变慢。
 如果是在gt box比较少（比如coco）或者inference阶段（?暂未确认inference对进入这段代码）建议注释此段代码（设置51行SKUData为false即可）。
--- my_config/skudet/skudet_faster_rcnn_r18_fpn_2x_191124.py
+++ my_config/skudet/skudet_faster_rcnn_r18_fpn_2x_191124.py
@@ -1,6 +1,6 @@
 # model settings
 model = dict(
-    type='RPN',
+    type='FasterRCNN',
     pretrained='torchvision://resnet18',
     backbone=dict(
         type='ResNet',
@@ -25,7 +25,25 @@ model = dict(
         target_stds=[1.0, 1.0, 1.0, 1.0],
         loss_cls=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0)))
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0)),
+    bbox_roi_extractor=dict(
+        type='SingleRoIExtractor',
+        roi_layer=dict(type='RoIAlign', out_size=7, sample_num=2),
+        out_channels=256,
+        featmap_strides=[4, 8, 16, 32]),
+    bbox_head=dict(
+        type='SharedFCBBoxHead',
+        num_fcs=2,
+        in_channels=256,
+        fc_out_channels=1024,
+        roi_feat_size=7,
+        num_classes=2,
+        target_means=[0., 0., 0., 0.],
+        target_stds=[0.1, 0.1, 0.2, 0.2],
+        reg_class_agnostic=False,
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)))
 # model training and testing settings
 train_cfg = dict(
     rpn=dict(
@@ -43,15 +61,42 @@ train_cfg = dict(
             add_gt_as_proposals=False),
         allowed_border=0,
         pos_weight=-1,
-        debug=False))
-test_cfg = dict(
-    rpn=dict(
+        debug=False),
+    rpn_proposal=dict(
         nms_across_levels=False,
         nms_pre=2000,
         nms_post=2000,
         max_num=2000,
         nms_thr=0.7,
-        min_bbox_size=0))
+        min_bbox_size=0),
+    rcnn=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.5,
+            min_pos_iou=0.5,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='RandomSampler',
+            num=512,
+            pos_fraction=0.25,
+            neg_pos_ub=-1,
+            add_gt_as_proposals=True),
+        pos_weight=-1,
+        debug=False))
+test_cfg = dict(
+    rpn=dict(
+        nms_across_levels=False,
+        nms_pre=1000,
+        nms_post=1000,
+        max_num=1000,
+        nms_thr=0.7,
+        min_bbox_size=0),
+    rcnn=dict(
+        score_thr=0.05, nms=dict(type='nms', iou_thr=0.5), max_per_img=100)
+    # soft-nms is also supported for rcnn testing
+    # e.g., nms=dict(type='soft_nms', iou_thr=0.5, min_score=0.05)
+)
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
@@ -59,13 +104,13 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_label=False),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -102,8 +147,8 @@ data = dict(
         pipeline=test_pipeline))
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
-# runner configs
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
@@ -123,7 +168,7 @@ log_config = dict(
 total_epochs = 21
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/skudet/skudet_rpn_r18_fpn_2x_191122'
+work_dir = './work_dirs/skudet/skudet_faster_rcnn_r18_fpn_2x_191124'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
